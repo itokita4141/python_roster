482787102d08a402151d067f80f0d18eac99d0f2
DESKTOP-5508BIE\user <itokita41@gmail.com>
2021-11-26 21:09:48 +0900

    sqlalchemyテーブル追加migration

--- app_roster/db/migrate/versions/001_add_migrate_users_table.py
+++ app_roster/db/migrate/versions/001_add_migrate_users_table.py
@@ -1,3 +1,15 @@
+# //////////////////////////////////////////////
+# userId = models.IntegerField(null=False, blank=False, primary_key=True, verbose_name="ユーザーID")
+# name = models.CharField(max_length=20, verbose_name="名前")
+# address = models.CharField(max_length=100, verbose_name="住所")
+# tell = models.CharField(max_length=20, null=True, blank=True, verbose_name="電話番号")
+# sex = models.CharField(max_length=1, verbose_name="性別(m:男性,f:女性)")
+# contract = models.CharField(max_length=100, null=False, blank=False, verbose_name="社員パートなど")
+# deleteFlag = models.BooleanField(default=True, verbose_name="削除フラグ")
+# updateTime = models.DateTimeField(blank=False, null=False, default=timezone.now, verbose_name="更新日時")
+# addTime = models.DateTimeField(blank=False, null=False, default=timezone.now, verbose_name="追加日時")
+# //////////////////////////////////////////////
+
 from sqlalchemy import *
 from migrate import *
 from sqlalchemy.dialects.mysql import BIGINT, VARCHAR, DATETIME, TEXT
@@ -17,17 +29,6 @@ table = Table(
     Column('deleteFlag', BOOLEAN, default=0),
     Column('updateTime', TIMESTAMP),
     Column('addTime', TIMESTAMP, server_default=current_timestamp()))
-
-# userId = models.IntegerField(null=False, blank=False, primary_key=True, verbose_name="ユーザーID")
-# name = models.CharField(max_length=20, verbose_name="名前")
-# address = models.CharField(max_length=100, verbose_name="住所")
-# tell = models.CharField(max_length=20, null=True, blank=True, verbose_name="電話番号")
-# sex = models.CharField(max_length=1, verbose_name="性別(m:男性,f:女性)")
-# contract = models.CharField(max_length=100, null=False, blank=False, verbose_name="社員パートなど")
-# deleteFlag = models.BooleanField(default=True, verbose_name="削除フラグ")
-# updateTime = models.DateTimeField(blank=False, null=False, default=timezone.now, verbose_name="更新日時")
-# addTime = models.DateTimeField(blank=False, null=False, default=timezone.now, verbose_name="追加日時")
-
 
 def upgrade(migrate_engine):
     meta.bind = migrate_engine
--- app_roster/db/migrate/versions/002_add_migrate_attendance_table.py
+++ app_roster/db/migrate/versions/002_add_migrate_attendance_table.py
@@ -1,3 +1,16 @@
+# //////////////////////////////////////////////
+# attendanceId = models.IntegerField(null=False, blank=False, primary_key=True, verbose_name="出勤ID")
+# userId = models.IntegerField(null=False, blank=False, verbose_name="ユーザーID")
+# yearMonth = models.CharField(max_length=6, null=True, blank=True, verbose_name="対象年月")
+# day = models.CharField(max_length=2, null=True, blank=True, verbose_name="対象日")
+# startTime = models.CharField(max_length=8, blank=True, null=True, verbose_name="勤務開始時間")
+# endDateTime = models.DateTimeField(blank=True, null=True, verbose_name="勤務終了時間")
+# restStartTime = models.DateTimeField(blank=True, null=True, verbose_name="休憩開始時間")
+# restEndTime = models.DateTimeField(blank=True, null=True, verbose_name="休憩終了時間")
+# deleteFlag = models.BooleanField(default=True, verbose_name="削除フラグ")
+# updateTime = models.DateTimeField(blank=True, null=True, default=timezone.now, verbose_name="更新時間")
+# addTime = models.DateTimeField(blank=True, null=True, default=timezone.now, verbose_name="追加時間")
+# //////////////////////////////////////////////
 from sqlalchemy import *
 from migrate import *
 from sqlalchemy.dialects.mysql import BIGINT, VARCHAR, DATETIME, TEXT
@@ -6,7 +19,7 @@ from sqlalchemy.dialects.mysql import TIMESTAMP as Timestamp
 from sqlalchemy.sql.functions import current_timestamp
 
 meta = MetaData()
-attendance_table = Table(
+table = Table(
     'migrate_attendance', meta,
     Column('attendanceId', BIGINT(unsigned=True), primary_key=True),
     Column('userId', BIGINT(unsigned=True)),
@@ -20,22 +33,10 @@ attendance_table = Table(
     Column('updateTime', TIMESTAMP),
     Column('addTime', TIMESTAMP, server_default=current_timestamp()))
 
-# attendanceId = models.IntegerField(null=False, blank=False, primary_key=True, verbose_name="出勤ID")
-# userId = models.IntegerField(null=False, blank=False, verbose_name="ユーザーID")
-# yearMonth = models.CharField(max_length=6, null=True, blank=True, verbose_name="対象年月")
-# day = models.CharField(max_length=2, null=True, blank=True, verbose_name="対象日")
-# startTime = models.CharField(max_length=8, blank=True, null=True, verbose_name="勤務開始時間")
-# endDateTime = models.DateTimeField(blank=True, null=True, verbose_name="勤務終了時間")
-# restStartTime = models.DateTimeField(blank=True, null=True, verbose_name="休憩開始時間")
-# restEndTime = models.DateTimeField(blank=True, null=True, verbose_name="休憩終了時間")
-# deleteFlag = models.BooleanField(default=True, verbose_name="削除フラグ")
-# updateTime = models.DateTimeField(blank=True, null=True, default=timezone.now, verbose_name="更新時間")
-# addTime = models.DateTimeField(blank=True, null=True, default=timezone.now, verbose_name="追加時間")
-
 def upgrade(migrate_engine):
     meta.bind = migrate_engine
-    attendance_table.create()
+    table.create()
 
 def downgrade(migrate_engine):
     meta.bind = migrate_engine
-    attendance_table.drop()
+    table.drop()
