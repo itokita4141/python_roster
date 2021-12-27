6da77f84081ad91e4003c514a0072e08f36d112e
DESKTOP-5508BIE\user <itokita41@gmail.com>
2021-12-28 05:42:29 +0900

    atlas mongoDB接続成功

--- app_roster/settings.py
+++ app_roster/settings.py
@@ -79,90 +79,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "prj_roster.wsgi.application"
 
-
-# Database
+############
+# Database #
+############
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE" : "django.db.backends.sqlite3",
-#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-#     }
-# }
 
 # MongoDB
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         # 'ENGINE': 'django.db.backends.',
-#         # 'ENGINE': 'django',
-#         'ENGINE': 'django_mongodb_engine',
-#         'NAME': 'admin',
-#         'USER': 'itokita41',
-#         'PASSWORD': 'itokita41pass',
-#         'HOST': 'cluster0-shard-00-02.tx265.mongodb.net',
-#         'PORT': '27017',
-#         'SSL': True,
-#     }
-# }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django_mongodb_engine',
-#         'NAME': 'rosterdb',
-#         'CLIENT': {
-#            # 'host': 'mongodb://{user_name}:{pass}@{ip_adress}:{port_num}/{your_dbname}',
-#            'host': 'mongodb://itokita41:itokita41pass@cluster0-shard-00-02.tx265.mongodb.net:27107/rosterdb',
-#         }
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'djongo',
-#         "NAME": 'admin',
-#         "CLIENT": {
-#             'USER': 'itokita41',
-#             'PASSWORD': 'itokita41pass',
-#             'HOST': 'cluster0-shard-00-02.tx265.mongodb.net',
-#             'PORT': '27017',
-#             'SSL': True,
-# # venv\Lib\site-packages\certifi
-#         },
-#     },
-# }
-
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',         # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'djongo',                              # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'rosterdb',                                # Or path to database file if using sqlite3.
-        'USER': 'itokita41',                               # Not used with sqlite3.
-        'PASSWORD': 'itokita41pass',                       # Not used with sqlite3.
-        'SSL': True,
-        'HOST': 'cluster0-shard-00-02.tx265.mongodb.net',  # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': 27017,                                     # Set to empty string for default. Not used with sqlite3.
-    }
+DATABASES= {
+    "default": {
+        "ENGINE": "djongo",
+        "NAME": "rosterdb",
+        "CLIENT": {
+            "host": "cluster0-shard-00-02.tx265.mongodb.net",
+            "port": 27017,
+            "username": "itokita41",
+            "password": "itokita41pass",
+            "ssl": "true",
+            "authSource": "admin",
+        },
+    },
 }
 
-# mongo cluster0-shard-00-02.tx265.mongodb.net:27017/admin --ssl -u itokita41 -p
-# DATABASES= {
-#         'default': {
-#             'ENGINE': 'djongo',
-#             'ENFORCE_SCHEMA': True
-#             'NAME': 'your-db-name',
-#             'HOST': 'host-name or ip address',
-#             'PORT': port_number,
-#             'USER': 'db-username',
-#             'PASSWORD': 'password',
-#             'AUTH_SOURCE': 'db-name',
-#             'AUTH_MECHANISM': 'SCRAM-SHA-1',
-#             'REPLICASET': 'replicaset',
-#             'SSL': 'ssl',
-#             'SSL_CERTFILE': 'ssl_certfile',
-#             'SSL_CA_CERTS': 'ssl_ca_certs',
-#             'READ_PREFERENCE': 'read_preference'
-#         }
-#     }
+
 # postgresSQL
 # DATABASES = {
 #     'default': {
@@ -175,6 +114,14 @@ DATABASES = {
 #     }
 # }
 
+
+# sqlite3
+# DATABASES = {
+#     "default": {
+#         "ENGINE" : "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+#     }
+# }
 
 
 # Password validation
