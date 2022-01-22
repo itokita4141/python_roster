bf7ace0892162e93efb67034ef410f4abe3605e0
DESKTOP-5508BIE\user <itokita41@gmail.com>
2022-01-22 20:00:14 +0900

    mongoClient設定中

--- app_roster/db/mongodb/config/setting_mongo_pymongo.py
+++ app_roster/db/mongodb/config/setting_mongo_pymongo.py
@@ -1,6 +1,7 @@
-# 参考 z
+# 参考
 # mongodb+srv://<username>:<password>@cluster0.tx265.mongodb.net/test
-# djongo://itokita41:itokita41pass@cluster0.tx265.mongodb.net:27017/rosterdb?authrizrDB=admin
+# https://qiita.com/chenglin/items/ecf6f67e8f80c4750204
+# djongo://itokita41:itokita41pass@cluster0.tx265.mongodb.net:27017/?rosterdb?authSource=admin
 from pymongo import MongoClient
 url = "djongo://itokita41:itokita41pass@cluster0.tx265.mongodb.net:27017"
 db = MongoClient.rosterdb
