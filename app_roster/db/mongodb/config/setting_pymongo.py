68fed9acd8858fab7e692c81e9c2ac04869e43f5
DESKTOP-5508BIE\user <itokita41@gmail.com>
2022-01-30 21:24:49 +0900

    ログイン作成

--- app_roster/db/mongodb/config/setting_pymongo.py
+++ app_roster/db/mongodb/config/setting_pymongo.py
@@ -1,28 +1,9 @@
-# 参考
-# mongodb+srv://<username>:<password>@cluster0.tx265.mongodb.net/test
-# https://qiita.com/chenglin/items/ecf6f67e8f80c4750204
-# mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[database][?options]]
-# djongo://itokita41:itokita41pass@cluster0.tx265.mongodb.net:27017/?rosterdb?authSource=admin
 from pymongo import MongoClient
-
-# url = "mongodb://itokita41:itokita41pass@cluster02.tx265.mongodb.net:27017/rosterdb?authSource=admin"
-# with MongoClient(url) as client:
-#     rosterdb = client.rosterdb
-#     app_roster_users = rosterdb.app_roster_users
-#     name = app_roster_users(db=rosterdb, filter={id: 1})
 
 class settingPymongo(object):
 
     def __init__(self, db_name, collection_name):
-        # url = "mongodb://itokita41:itokita41pass@cluster02.tx265.mongodb.net:27017/rosterdb?authSource=admin"
-        # url = "mongodb://itokita41:itokita41pass@cluster0-shard-00-02.tx265.mongodb.net"
-        # self.client = MongoClient(url, 27017)
-        # self.username = 'itokita41'
-        # self.password = 'itokita41pass'
-        # self.authSource = 'admin'
-        # self.authMechanism = 'SCRAM-SHA-256'
-        # uri = 'mongodb://itokita41:itokita41pass@cluster0-shard-00-02.tx265.mongodb.net:27017/rosterdb?ssl=false&authSource=admin'
-        uri = 'mongodb://itokita41:itokita41pass@cluster0-shard-00-02.tx265.mongodb.net'
+        uri = 'mongodb://itokita41:itokita41pass@cluster0-shard-00-01.tx265.mongodb.net'
         self.client = MongoClient(uri,
                                   port=27017,
                                   ssl=True,
@@ -31,18 +12,6 @@ class settingPymongo(object):
                                   authSource='admin'
                                   # authMechanism='SCRAM-SHA-256'
                                   )
-        # con = MongoClient(uri, port=27017)
-        # db = con.rosterdb
-        # print(db)
-        # collections = db.app_roster_users
-        # print(collections)
-        # found = collections.find({'app_roster_users': True})  # find returns all entries
-        # print(found)
-        # for i in found:
-        #     print(i)
-        #
-        # self.db = self.client[db_name]  # DB名を設定
-        # # self.client = MongoClient(url, 27017)
         self.ssl = True
         self.username = 'itokita41'
         self.password = 'itokita41pass'
@@ -62,21 +31,3 @@ class settingPymongo(object):
 
     def list_collection_names(self):
         return self.db.list_collection_names()
-
-        # # pymongo
-        # mongo = settingPymongo('rosterdb', 'app_roster_users')
-        # print('-----------------find_One-----------------')
-        # print(type(findOne))
-        # print(findOne)
-        #
-        # find = mongo.find()
-        # print('-------------------find-------------------')
-        # print(type(find))
-        # for doc in find:
-        #     print(doc)
-
-    # def collection_names(self):
-    #     return self.db.collection_names()
-    #
-    # def list_collection_names(self):
-    #     return self.db.list_collection_names()
--- app_roster/view/login_view.py
+++ app_roster/view/login_view.py
@@ -12,8 +12,6 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-# from .models import user, attendance
-# from ..models import Question, Choice
 from ..models import Users, Attendances, Logs
 # sql alchemy
 from sqlalchemy import create_engine, Column, Integer, String
@@ -23,14 +21,8 @@ from sqlalchemy.orm.exc import NoResultFound
 import sys
 from setting_alchemy import Base
 from setting_alchemy import ENGINE
-# from setting_alchemy import DATABASE
 from setting_alchemy import session
-# sys.path.append("/app_roster/db/model")
-# from users import *
-# from attendances import *
-# mongodb read
 from rest_framework import viewsets
-# sys.path.append("/app_roster")
 from app_roster.seliarizers import UsersSerializer, AttendancesSerializer,LogsSerializer
 # pymongo
 from pymongo import MongoClient
@@ -48,162 +40,111 @@ def ajax_roster_login(request):
         'errorMessage': 'dummy'
     }
     # フォーム入力のユーザーID・パスワード取得
-    # uid = request.POST.get('txt_userid')
-    # pwd = request.POST.get('txt_password')
-    # json_data = request.POST["uid"]
-    # json_data = request.POST["pwd"]
-    # data = json.dumps(json_data)
     uid = request.POST.get("uid", False)
     pwd = request.POST.get("pwd", False)
-
     if request.method == 'POST':
-        # 未入力がある場合は抜ける
-        # if len(uid) == 0 or len(pwd) == 0:
-        #     returnMessageParams["errorMessage"] = "ユーザーIDまたはパスワードが入力されていません。<br/>入力してください。"
-        #     return render(request, "../../login", returnMessageParams)
         print("〓〓〓〓〓〓〓")
         print("ユーザーID="+uid)
         print("password="+pwd)
         print("〓〓〓〓〓〓〓")
-
         # pymongo
-        # test = settingPymongo('rosterdb', 'app_roster_attendances')
-        # collectionsnames = test.collection_names()
-        # listcollectionnames = test.list_collection_names()
-        # mongo = settingPymongo('rosterdb', 'app_roster_users')
         mongo = settingPymongo('rosterdb', 'app_roster_users')
-        findone = mongo.find_one()
-        print('-----------------find_One-----------------')
-        print(type(findone))
-        print(findone)
-
-        find = mongo.find()
-        print('-------------------find-------------------')
-        print(type(find))
-        for doc in find:
-            print(doc)
-
-        print('-----------------find filter-----------------')
-        find = mongo.find(filter={'id': 3})
-        for doc in find:
-            print(doc)
-            print(doc['name'])
-
-        print('-----------------find filters-----------------')
-        find = mongo.find(filter={'id':3, 'name': 'taka'})
-        print(find.count()) # 件数
-        for doc in find:
-            print(doc)
-
-        print('-----------------sort-------------------------')
-        find = mongo.find(sort=[('id', ASCENDING)])
-        for doc in find:
-            print(doc)
-
-        print('-----------------cursol sort-------------------------')
-        find = mongo.find()
-      # for doc in find.sort([('salary', DESCENDING), ('name', ASCENDING)]):
-        for doc in find.sort([('name', DESCENDING)]):
-            print(doc)
-
-        # mongo = settingPymongo('rosterdb')
-        # print(mongo.collection_names())
-        # print(mongo.list_collection_names())
-
-        # url = "mongodb://itokita41:itokita41pass@cluster02.tx265.mongodb.net:27017/rosterdb?authSource=admin"
-        # with MongoClient(url) as client:
-        #     rosterdb = client.rosterdb
-        #     app_roster_users = rosterdb.app_roster_users
-        #     name = app_roster_users(db=rosterdb, filter={id: 1})
-
-        # mongoengine
-        # usersData = Users.find_all()
-        # # print(Users.objects.all())
-        print(Users.objects.all())
-        usersAllData = Users.objects.all()
-        # print("Usersのデータ数=" . str(usersAllData))
-        counter = 0
-        for usersData in usersAllData:
-            print(counter)
-            print(usersData.userId)
-            counter = counter + 1
-
-        usersListCount = counter
-        # mongoalchemy
-        # ・flaskのみ？
-        # userテーブル読み込み
-        # usersListCount = session.query(Users).filter(Users.email == uid, Users.password == pwd).count()
-        # usersListCount2 = session.query(Users).all()
-        # print("=======================")
-        print("users.count="+str(usersListCount))
-        # print("=======================")
+        find = mongo.find(filter={'name': uid, 'password': pwd})
+        usersListCount = find.count()
+        print('count=='+str(usersListCount))
+        try:
+            doc = find.next
+            for doc in find:
+                print(doc['id'])
+                print(doc['name'])
+                print(doc['password'])
+        except StopIteration:
+            pass
         # ログインチェック
         if (usersListCount == 1):
             returnMessageParams = {
                 'errorMessage': '',
                 'result': 'ok',
             }
-            # returnMessageParams["errorMessage"] = ''
-            # redirect("../../input")
             return JsonResponse(returnMessageParams)
         if usersListCount == 0:
             returnMessageParams = {
                 'errorMessage': 'ログインIDまたはパスワードが間違っています',
                 'result': 'ng',
             }
-            # returnMessageParams["errorMessage"] = 'ログインIDまたはパスワードが間違っています。'
             render(request, "roster_login.html", returnMessageParams)
             return JsonResponse(returnMessageParams)
-            # return render(request, "roster_login.html", {'data_json': json.dumps(returnMessageParams)})
-            # return redirect(request.META['HTTP_REFERER']) # 元のURLに戻す
         elif(usersListCount > 1):
             returnMessageParams = {
                 'errorMessage': '複数ユーザーのログインID、パスワードが一致しています。' \
                                 '<br/>データ不正のため運営に連絡してください。<br/>itokita41@gmail.com',
                 'result': 'ng',
             }
-            # returnMessageParams["errorMessage"] = '複数ユーザーのログインID、パスワードが一致しています。' \
-            #                                       '<br/>データ不正のため運営に連絡してください。<br/>itokita41@gmail.com'
             render(request, "roster_login.html", returnMessageParams)
             return JsonResponse(returnMessageParams)
-            # return render(request, "roster_login.html", {'data_json': json.dumps(returnMessageParams)})
-            # return redirect(request.META['HTTP_REFERER']) # 元のURLに戻す
     elif request.method == 'GET':
         print("通信に失敗しました。GET送信になっています。")
 
-
-# def roster_login(request):
-#     returnMessageParams = {
-#         'errorMessage': 'dummy'
-#     }
-#     # フォーム入力のユーザーID・パスワード取得
-#     uid = request.GET.get('txt_userid')
-#     pwd = request.GET.get('txt_password')
-#     # 未入力がある場合は抜ける
-#     # if len(uid) == 0 or len(pwd) == 0:
-#     #     returnMessageParams["errorMessage"] = "ユーザーIDまたはパスワードが入力されていません。<br/>入力してください。"
-#     #     return render(request, "../../login", returnMessageParams)
-#     print("〓〓〓〓〓〓〓")
-#     print("ユーザーID="+uid)
-#     print("password="+pwd)
-#     print("〓〓〓〓〓〓〓")
-#     # userテーブル読み込み
-#     usersListCount = session.query(Users).filter(Users.email == uid, Users.password == pwd).count()
-#     # usersListCount2 = session.query(Users).all()
-#     print("=======================")
-#     print("users.count="+str(usersListCount))
-#     print("=======================")
-#     # ログインチェック
-#     if (usersListCount == 1):
-#         return redirect("../../input")
-#     if usersListCount == 0:
-#         returnMessageParams["errorMessage"] = 'ログインIDまたはパスワードが間違っています。'
-#         return render(request, "roster_login.html", {'data_json': returnMessageParams})
-#         # return render(request, "roster_login.html", {'data_json': json.dumps(returnMessageParams)})
-#         # return redirect(request.META['HTTP_REFERER']) # 元のURLに戻す
-#     elif(usersListCount > 1):
-#         returnMessageParams["errorMessage"] = '複数ユーザーのログインID、パスワードが一致しています。' \
-#                                               '<br/>データ不正のため運営に連絡してください。<br/>itokita41@gmail.com'
-#         return render(request, "roster_login.html", {'data_json': returnMessageParams})
-#         # return render(request, "roster_login.html", {'data_json': json.dumps(returnMessageParams)})
-#         # return redirect(request.META['HTTP_REFERER']) # 元のURLに戻す
+#   # 〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
+#   # 処理参考用 ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+#   # 〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
+#   findone = mongo.find_one()
+#   print('-----------------find_One-----------------')
+#   print(type(findone))
+#   print(findone)
+#
+#   find = mongo.find()
+#   print('-------------------find-------------------')
+#   print(type(find))
+#   for doc in find:
+#       print(doc)
+#   print('-----------------find filter-----------------')
+#   find = mongo.find(filter={'id': 3})
+#   for doc in find:
+#       print(doc)
+#       print(doc['name'])
+#   print('-----------------find filters-----------------')
+#   find = mongo.find(filter={'id':3, 'name': 'taka'})
+#   print(find.count()) # 件数
+#   for doc in find:
+#       print(doc)
+#   print('-----------------sort-------------------------')
+#   find = mongo.find(sort=[('id', ASCENDING)])
+#   for doc in find:
+#       print(doc)
+#   print('-----------------cursol sort-------------------------')
+#   find = mongo.find()
+# # for doc in find.sort([('salary', DESCENDING), ('name', ASCENDING)]):
+# #   for doc in find.sort([('name', DESCENDING)]):
+# #       print(doc)
+#   print('-----------------nextループ-------------------------')
+#   try:
+#       doc = find.next()
+#       while doc != None:
+#           print(doc)
+#           doc = find.next()
+#   except StopIteration:
+#       pass
+#
+#   # mongoengine
+#   print(Users.objects.all())
+#   usersAllData = Users.objects.all()
+#   # print("Usersのデータ数=" . str(usersAllData))
+#   counter = 0
+#   for usersData in usersAllData:
+#       print(counter)
+#       print(usersData.userId)
+#       counter = counter + 1
+#
+#   usersListCount = counter
+#   # mongoalchemy
+#   # ・flaskのみ？
+#   # userテーブル読み込み
+#   # usersListCount = session.query(Users).filter(Users.email == uid, Users.password == pwd).count()
+#   # usersListCount2 = session.query(Users).all()
+#   # print("=======================")
+#   print("users.count="+str(usersListCount))
+# print("=======================")
+#   # 〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
+#   # 処理参考用 ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+#   # 〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓〓
